b661a72de64e4a87b64a024ded618a0e872e4d86
Arkadi Chubrik <arkadi@chubrik.org>
2020-02-23 00:55:08 +0300

    Fix placeholder in Safari

--- Toponym.Site/Extensions/RequestExtensions.cs
+++ Toponym.Site/Extensions/RequestExtensions.cs
@@ -32,5 +32,28 @@ namespace Toponym.Site.Extensions
             var userAgent = request.Headers["User-Agent"].Single();
             return userAgent.Contains("iPhone") || userAgent.Contains("iPad") || userAgent.Contains("iPod");
         }
+
+        // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/#26358856
+        public static string BrowserName(this HttpRequest request)
+        {
+            var userAgent = request.Headers["User-Agent"].Single();
+
+            if (userAgent.Contains("Opera") || userAgent.Contains("OPR"))
+                return "opera";
+
+            if (userAgent.Contains("Chrome"))
+                return "chrome";
+
+            if (userAgent.Contains("Firefox"))
+                return "firefox";
+
+            if (userAgent.Contains("Safari"))
+                return "safari";
+
+            if (userAgent.Contains("MSIE") || userAgent.Contains("Trident"))
+                return "msie";
+
+            return "unknown";
+        }
     }
 }
